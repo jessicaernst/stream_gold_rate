dd6ed443fef301075e12b5f9eda61496e294de11
Belladonnixi <jessica_ernst@outlook.de>
2025-03-24 10:35:28 +0100

    feature: add solved tasksheet tasks

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
-class GoldScreen extends StatelessWidget {
+class GoldScreen extends StatefulWidget {
   const GoldScreen({super.key});
+
+  @override
+  State<GoldScreen> createState() => _GoldScreenState();
+}
+
+class _GoldScreenState extends State<GoldScreen> {
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -21,14 +24,33 @@ class GoldScreen extends StatelessWidget {
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
+              // Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
+              StreamBuilder<double>(
+                stream: getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // Zustand: Ladend
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasData) {
+                    // Zustand: Daten (Goldpreis vorhanden)
+                    final goldPrice = snapshot.data!;
+                    return Text(
+                      NumberFormat.simpleCurrency(locale: 'de_DE')
+                          .format(goldPrice),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineLarge!
+                          .copyWith(
+                              color: Theme.of(context).colorScheme.primary),
+                    );
+                  } else if (snapshot.hasError) {
+                    // Zustand: Error
+                    return Text('Fehler: ${snapshot.error}');
+                  } else {
+                    // Zustand: Keine Daten und kein Fehler (sollte hier eigentlich nicht eintreten)
+                    return const Text('Keine Daten vorhanden');
+                  }
+                },
               ),
             ],
           ),
